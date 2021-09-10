6980cf03b34551ec4ea4fc202cf73fa99b0170e0
veyselusta <vyslusta@gmail.com>
2021-09-10 13:52:30 +0300

    Initial commit

---
+++ my-promise.js
@@ -0,0 +1,41 @@
+const states = {
+  PENDING: 'pending',
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected'
+};
+
+class LLJSPromise {
+  constructor(computation) {
+    this._state = states.PENDING;
+
+    this._value = undefined;
+    this._reason = undefined;
+
+    this._thenQueue = [];
+    this._finallyQueue = [];
+
+    if (typeof computation === 'function') {
+      setTimeout(() => {
+        try {
+          computation(
+            this._onFulfilled.bind(this),
+            this._onRejected.bind(this)
+          );
+        } catch (ex) {
+          this._onRejected(ex);
+        }
+      });
+    }
+  }
+
+  then() {}
+
+  catch() {}
+
+  finally() {}
+
+  _onFulfilled() {}
+
+  _onRejected(reason) {}
+
+}
