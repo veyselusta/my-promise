155e068cdb6f10c18223ff1d4426a4e8521958e0
veyselusta <vyslusta@gmail.com>
2021-09-10 13:54:55 +0300

    Complete the class to make a copy of promise object

--- my-promise.js
+++ my-promise.js
@@ -4,6 +4,8 @@ const states = {
   REJECTED: 'rejected'
 };
 
+const isThenable = maybePromise => maybePromise && typeof maybePromise.then === 'function';
+
 class LLJSPromise {
   constructor(computation) {
     this._state = states.PENDING;
@@ -28,14 +30,107 @@ class LLJSPromise {
     }
   }
 
-  then() {}
+  then(fulfilledFn, catchFn) {
+    const controlledPromise = new LLJSPromise();
+    this._thenQueue.push([controlledPromise, fulfilledFn, catchFn]);
 
-  catch() {}
+    if (this._state === states.FULFILLED) {
+      this._propagateFulfilled();
+    } else if (this._state === states.REJECTED) {
+      this._propagateRejected();
+    }
 
-  finally() {}
+    return controlledPromise;
+  }
 
-  _onFulfilled() {}
+  catch(catchFn) {
+    return this.then(undefined, catchFn);
+  }
 
-  _onRejected(reason) {}
+  finally(sideEffectFn) {
+    if (this._state !== states.PENDING) {
+      sideEffectFn();
+
+      return this._state === states.FULFILLED
+      ? LLJSPromise.resolve(this._value)
+      : LLJSPromise.reject(this._reason)
+    }
+
+    const controlledPromise = new LLJSPromise()
+    this._finallyQueue.push([controlledPromise, sideEffectFn]);
+
+    return controlledPromise;
+  }
+
+  _propagateFulfilled() {
+    this._thenQueue.forEach(([controlledPromise, fulfilledFn]) => {
+      if (typeof fulfilledFn === 'function') {
+        const valueOrPromise = fulfilledFn(this._value);
+
+        if (isThenable(valueOrPromise)) {
+          valueOrPromise.then(
+            value => controlledPromise._onFulfilled(value),
+            reason => controlledPromise._onRejected(reason)
+          );
+        } else {
+          controlledPromise._onFulfilled(valueOrPromise);
+        }
+      } else {
+        return controlledPromise._onFulfilled(this._value);
+      }
+    });
+
+    this._finallyQueue.forEach(([controlledPromise, sideEffectFn]) => {
+      sideEffectFn();
+      controlledPromise._onFulfilled(this._value);
+    });
+
+    this._thenQueue = [];
+    this._finallyQueue = [];
+  }
+
+  _propagateRejected() {
+    this._thenQueue.forEach(([controlledPromise, _, catchFn]) => {
+      if (typeof catchFn === 'function') {
+        const valueOrPromise = catchFn(this._reason);
+
+        if (isThenable(valueOrPromise)) {
+          valueOrPromise.then(
+            value => controlledPromise._onFulfilled(value),
+            reason => controlledPromise._onRejected(reason)
+          );
+        } else {
+          controlledPromise._onFulfilled(valueOrPromise);
+        }
+      } else {
+        return controlledPromise._onRejected(this._reason);
+      }
+    });
+
+    this._finallyQueue.forEach(([controlledPromise, sideEffectFn]) => {
+      sideEffectFn();
+      controlledPromise._onRejected(this._value);
+    });
+
+    this._thenQueue = [];
+    this._finallyQueue = [];
+  }
+
+
+  _onFulfilled(value) {
+    if (this._state === states.PENDING) {
+      this._state = states.FULFILLED;
+      this._value = value;
+      this._propagateFulfilled();
+    }
+  }
+
+  _onRejected(reason) {
+    if (this._state === states.PENDING) {
+      this._state = states.REJECTED;
+      this._reason = reason;
+      this._propagateRejected();
+    }
+  }
 
 }
